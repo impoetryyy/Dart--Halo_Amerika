e6ecd4bab21a700eaa75a668471adb0639a8fff3
Putri Meliana <96984201+impoetryyy@users.noreply.github.com>
2023-10-24 17:13:18 +0700

    Add files via upload

---
+++ bin/halo_amerika.dart
@@ -0,0 +1,9 @@
+import 'package:halo_amerika/halo_amerika.dart' as halo_amerika;
+
+void main(List<String> arguments) {
+  print("Halo Amerika");
+  print("");
+  print("Selamat Malam di Amerika");
+  print("");
+  print("Amerika Penghasil Film MArvel");
+}
---
+++ test/halo_amerika_test.dart
@@ -0,0 +1,8 @@
+import 'package:halo_amerika/halo_amerika.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
